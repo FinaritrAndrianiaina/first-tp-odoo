a30fd233c2400f3f7909d932ef15d4e7d35afa4b
Finaritra Haritiana Andrianiaina <finaritrandrianiaina@gmail.com>
2022-02-18 13:10:16 +0300

    adding tags

--- addons/cours/models/cours_model.py
+++ addons/cours/models/cours_model.py
@@ -8,3 +8,10 @@ class Cours(models.Model):
     date_de_debut = fields.Date()
     date_de_fin = fields.Date()
     enseignant = fields.Many2one('res.partner',ondelete="cascade",string="Enseignant")
+    tags = fields.Many2many("category.category","tags_to_category","cours_id","category_id","Tags")
+
+class Category(models.Model):
+    _name="category.category"
+
+    name = fields.Char("Category")
+    tags = fields.Many2many("cours.cours","tags_to_category","category_id","cours_id","Cours")
